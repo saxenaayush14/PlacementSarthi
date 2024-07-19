7e66da57aecc2a48e197c1f5c44891c7da9e72b7
Ayush Saxena <ayushsaxena1405@gmail.com>
2024-07-19 17:04:29 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#15202B', top: 0, justifyContent: "space-between" }}>
+    <h1>Placement Pros</h1>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
---
+++ src/utils/constants.js
@@ -0,0 +1,43 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import MediationIcon from '@mui/icons-material/Mediation';
+import CellTowerIcon from '@mui/icons-material/CellTower';
+import StorageIcon from '@mui/icons-material/Storage';
+import HtmlIcon from '@mui/icons-material/Html';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import AndroidIcon from '@mui/icons-material/Android';
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+
+  { name: 'Computer Networks  gate smashers neso academy  ', icon: <CellTowerIcon />, name2:'Computer Networks',},
+  { name: 'DBMS               gate smashers', icon: <StorageIcon />,name2:'DBMS', },
+  { name: 'DSA Begginer       love babbar array basics', icon: <MediationIcon />,name2:'DSA Begginer', },
+  { name: 'DSA Advanced       striver dp', icon: <CodeIcon />, name2:'DSA Advanced',},
+  { name: 'Web Dev Begginer    html css', icon: <HtmlIcon />,name2:'Web Dev Begginer', },
+  { name: 'Web Dev Frontend     react js', icon: <GraphicEqIcon />, name2:'Web Dev Frontend',},
+  { name: 'Web Dev Backend      nodejs mongo db', icon: <JavascriptIcon />,name2:'Web Dev Backend', },
+  { name: 'OOPS                concepts', icon: <CodeIcon />,name2:'OOPS', },
+  { name: 'System Design       LLD HLD', icon: <SportsEsportsIcon />,name2:'System Design', },
+  { name: 'Aptitude           for placement', icon: <PsychologyIcon />,name2:'Aptitude', },
+  { name: 'Android Devlopment    kotlin', icon: <AndroidIcon />,name2:'Android Devlopment', },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
